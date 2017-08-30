547e0926066a78a55f29b7d0be25a4faf0481026
lanxing <569767186@qq.com>
2017-08-30 18:34:09 +0800

    the god parts

---
+++ javascript/API.md
@@ -0,0 +1,5 @@
+## Object
+
+判断标识符是否是 obj 的成员：
+
+    obj.hasOwnProperty(属性名) => true / false
--- javascript/typescript/test.js
+++ javascript/typescript/test.js
@@ -1,23 +1,16 @@
-var Person = (function () {
-    function Person(name) {
-        this.name = name;
+var Grid = (function () {
+    function Grid(scale) {
+        this.scale = scale;
     }
-    Person.prototype.sayHello = function () {
-        console.log("Hello, my name is " + this.name);
+    Grid.prototype.calculateDistanceFromOrigin = function (point) {
+        var xDist = (point.x - Grid.origin.x);
+        var yDist = (point.y - Grid.origin.y);
+        return Math.sqrt(xDist * xDist + yDist * yDist) / this.scale;
     };
-    return Person;
+    Grid.origin = { x: 0, y: 0 };
+    return Grid;
 }());
-// class Employee extends Person {
-//     private department: string;
-//     constructor(name: string, department: string) {
-//         super(name);
-//         this.department = department;
-//     }
-//     public getElevatorPitch() {
-//         return `Hello, my name is ${this.name} and I work in ${this.department}.`;
-//     }
-// }
-var howard = new Person('Howard');
-var jack = new Person('Jack');
-howard.sayHello();
-jack.sayHello();
+var grid1 = new Grid(1.0);
+var grid2 = new Grid(5.0);
+console.log(grid1.calculateDistanceFromOrigin({ x: 10, y: 10 }));
+console.log(grid2.calculateDistanceFromOrigin({ x: 10, y: 10 }));
